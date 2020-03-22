4c5c5be4d02ad0ba4ddbce4432d033e23369d6f3
Ryabchuk Anton <ryabchuk.anton@yandex.ru>
2020-03-22 20:28:07 +0300

    The basis of the project is created.

---
+++ yatube/posts/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Post
+
+
+
+class PostAdmin(admin.ModelAdmin):
+    # перечисляем поля, которые должны отображаться в админке
+    list_display = ("pk", "text", "pub_date", "author") 
+    # добавляем интерфейс для поиска по тексту постов
+    search_fields = ("text",) 
+    # добавляем возможность фильтрации по дате
+    list_filter = ("pub_date",) 
+    empty_value_display = "-пусто-"
+
+admin.site.register(Post, PostAdmin)
