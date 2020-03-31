e5058e056e3c4dad64934dc8fcac7a41dd893bd9
Ryabchuk Anton <ryabchuk.anton@yandex.ru>
2020-03-31 11:48:44 +0300

    Добавлен модуль Group

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,17 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post
+from .models import Post, Group
 
 
 
 class PostAdmin(admin.ModelAdmin):
     # перечисляем поля, которые должны отображаться в админке
-    list_display = ("pk", "text", "pub_date", "author") 
+    list_display = ("pk", "text", "pub_date", "author", "group") 
     # добавляем интерфейс для поиска по тексту постов
     search_fields = ("text",) 
     # добавляем возможность фильтрации по дате
     list_filter = ("pub_date",) 
     empty_value_display = "-пусто-"
 
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ("pk", "title", "slug", "description",)
+    empty_value_display = "-пусто-"
+
 admin.site.register(Post, PostAdmin)
+admin.site.register(Group, GroupAdmin)
